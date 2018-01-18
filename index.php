cb18faa56f99796a9b178e31d83c55b4226d74b0
tmccarten <thomasmccarten@hotmail.com>
2018-01-18 16:11:37 +0000

    setup folder structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en-GB">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>UI Engineer Exercise</title>
+    <meta name="description" content="Responsive Churchill advert built with HTML and Sass/CSS">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+
+
+  </body>
+</html>
