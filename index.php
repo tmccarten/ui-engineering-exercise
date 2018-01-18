06fcb501933d0e58e57f01f5270d2a0ef1c28bad
tmccarten <thomasmccarten@hotmail.com>
2018-01-18 17:07:03 +0000

    add initial html

--- index.php
+++ index.php
@@ -15,7 +15,30 @@
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
+    <main>
 
+      <div class="component">
+
+        <p class="slogan">Your dependable partner</p>
+
+        <ul class="checklist">
+          <li class="checklist__item">At Churchill we believe in being there for you.</li>
+          <li class="checklist__item">It's important that we live this through our policies.</li>
+          <li class="checklist__item">We believe every little interaction matters.</li>
+          <li class="checklist__item">Churchill spends his time making sure you're happy. Oh Yes!</li>
+        </ul>
+
+        <div class="image">
+          <img src="assets/img/churchill-dog.png" class="image__img" width="318" height="429" alt="The Churchill Dog in a decorative golden photo frame.">
+        </div>
+
+        <div class="speech-bubble">
+          <p class="speech-bubble__text">...Oh Yes!</p>
+        </div>
+
+      </div><!-- component ends -->
+
+    </main>
 
   </body>
 </html>
